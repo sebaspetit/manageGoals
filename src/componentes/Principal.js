3ec1144c2b93055d8e6120e36d934f165259b572
sebaspetit <sebastianpetit74@gmail.com>
2023-02-26 13:15:22 -0600

    test

--- src/componentes/Principal.js
+++ src/componentes/Principal.js
@@ -5,6 +5,7 @@ function Principal({ children }) {
         <sidenav>
             <a href="/lista">lista</a>
             <a href="/crear">crear</a>
+            <a href="/hola">saludo</a>
         </sidenav>
         <main>
             {children}
